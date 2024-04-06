2fe4fe86a700e3157f7742f9ec030f6eff897279
kcbojanowski <k1bojanowski@gmail.com>
2024-04-06 23:51:23 +0200

    cli update, testdata added

--- cmd/main.go
+++ cmd/main.go
@@ -4,45 +4,79 @@ import (
     "fmt"
     "io/ioutil"
     "os"
-    "github.com/kcbojanowski/remitly-json-verifier/pkg/verifier"
+    "path/filepath"
+    "strings"
     "github.com/manifoldco/promptui"
+    "github.com/kcbojanowski/remitly-json-verifier/pkg/verifier"
 )
 
 func main() {
-    prompt := promptui.Prompt{
-        Label: "Enter path to the JSON file",
-        Validate: func(input string) error {
-            if input == "" {
-                return fmt.Errorf("file path cannot be empty")
-            }
-            if _, err := os.Stat(input); os.IsNotExist(err) {
-                return fmt.Errorf("file does not exist")
-            }
-            return nil
-        },
+    prompt := promptui.Select{
+        Label: "Select Mode",
+        Items: []string{"Test with internal data", "Input your own JSON file"},
     }
 
-    filePath, err := prompt.Run()
+    _, result, err := prompt.Run()
+
     if err != nil {
         fmt.Printf("Prompt failed %v\n", err)
         return
     }
 
-    jsonBytes, err := ioutil.ReadFile(filePath)
-    if err != nil {
-        fmt.Printf("Error reading JSON file: %s\n", err)
-        return
-    }
+    switch result {
+    case "Test with internal data":
+        testdataPath := "./testdata"
+        files, err := ioutil.ReadDir(testdataPath)
+        if err != nil {
+            fmt.Println("Error reading test data directory:", err)
+            os.Exit(1)
+        }
 
-    valid, err := verifier.VerifyPolicyJSON(jsonBytes)
-    if err != nil {
-        fmt.Printf("Error verifying JSON: %s\n", err)
-        return
-    }
+        for _, f := range files {
+            if strings.HasSuffix(f.Name(), ".json") {
+                fmt.Printf("\n--- Testing %s:\n", f.Name())
+                testJSON, err := ioutil.ReadFile(filepath.Join(testdataPath, f.Name()))
+                if err != nil {
+                    fmt.Printf("Error reading %s: %s ❌\n", f.Name(), err)
+                    continue
+                }
 
-    if valid {
-        fmt.Println("The JSON policy is valid.")
-    } else {
-        fmt.Println("The JSON policy is not valid: contains a resource with a single asterisk.")
+                valid, err := verifier.VerifyPolicyJSON(testJSON)
+                if err != nil {
+                    fmt.Printf("Error verifying %s: %s ❌\n", f.Name(), err)
+                } else if valid {
+                    fmt.Printf("%s is valid. ✅\n", f.Name())
+                } else {
+                    fmt.Printf("%s is not valid. ❌\n", f.Name())
+                }
+                fmt.Println("\n")
+            }
+        }
+
+    case "Input your own JSON file":
+        prompt := promptui.Prompt{
+            Label: "Enter path to the JSON file",
+        }
+
+        filePath, err := prompt.Run()
+        if err != nil {
+            fmt.Printf("Prompt failed %v\n", err)
+            return
+        }
+
+        jsonBytes, err := ioutil.ReadFile(filePath)
+        if err != nil {
+            fmt.Printf("Error reading JSON file: %s ❌\n", err)
+            return
+        }
+
+        valid, err := verifier.VerifyPolicyJSON(jsonBytes)
+        if err != nil {
+            fmt.Printf("Error verifying JSON: %s ❌\n", err)
+        } else if valid {
+            fmt.Println("The JSON policy is valid. ✅")
+        } else {
+            fmt.Println("The JSON policy is not valid: contains a resource with a single asterisk. ❌")
+        }
     }
 }
